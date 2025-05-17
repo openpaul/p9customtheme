b86d6fd33df2a3e7a08b88d92ec5aa63e00eeeb3
Paul Saary <github@paulsaary.de>
2025-05-17 12:22:16 +0200

    minimal plotnine theme

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+import tempfile
+from pathlib import Path
+
+from matplotlib.testing.compare import compare_images
+from plotnine import ggplot
+
+LOCAL_FOLDER = Path(__file__).parent
+BASELINE_IMAGES = LOCAL_FOLDER / "baseline"
+
+
+# Simplified and not as robust plotnine comparison
+# Should be enough for this theme for now
+def ggplot_equals(plot: ggplot, name: str) -> bool:
+    reference = BASELINE_IMAGES / f"{name}.png"
+    if not reference.exists():
+        plot.save(reference)
+        raise FileNotFoundError(
+            f"Reference image {reference} does not exist, this is now the new reference! Check it and rerun"
+        )
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        tmpfile = Path(tmpdirname) / "test.png"
+        plot.save(tmpfile)
+        return compare_images(str(reference), str(tmpfile), tol=1e-3) is None
+
+
+ggplot.__eq__ = ggplot_equals  # type: ignore # to make the monkey patching work
